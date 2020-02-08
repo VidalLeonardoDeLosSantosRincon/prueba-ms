cecccd5e7cbe347d2f9b0406704dad8e50c88e19
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2020-02-08 06:18:32 -0400

    route to home commented on routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -72,7 +72,8 @@ $routes->setAutoRoute(true);
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+/*$routes->get('/', 'Home::index');*/ //commented by me
+$routes->get('/', 'Product::index');
 
 /**
  * --------------------------------------------------------------------
